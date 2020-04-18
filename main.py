fe33982b8723e9ea6d1f91b2019a20f8db3c79ce
ErikSugawara <erik.sugawara@grad.ufsc.br>
2020-04-18 17:40:32 -0300

    KDA and Name index

--- main.py
+++ main.py
@@ -3,69 +3,75 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-api_key = 'RGAPI-6c3bdf80-789e-463b-8f13-6984b19fee1e'
-watcher = LolWatcher(api_key)
-my_region = 'br1'
+def main():
+    # api_key = input("Insert your API Key: ")
+    api_key = 'RGAPI-6c3bdf80-789e-463b-8f13-6984b19fee1e'
+    # summoner_name = input("Insert your summoner name: ")
+    my_region = 'br1'
+    watcher = LolWatcher(api_key)
+    me = watcher.summoner.by_name(my_region, 'Magafo')
+    print(me)
 
-me = watcher.summoner.by_name(my_region, 'Magafo')
-print(me)
+    # my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
 
-my_ranked_stats = watcher.league.by_summoner(my_region, me['id'])
-print(my_ranked_stats)
+    my_matches = watcher.match.matchlist_by_account(my_region, me['accountId'])
 
-my_matches = watcher.match.matchlist_by_account(my_region, me['accountId'])
-# print(my_matches)
+    last_match = my_matches['matches'][0]
 
-last_match = my_matches['matches'][0]
-print(last_match)
+    match_detail = watcher.match.by_id(my_region, last_match['gameId'])
+    summoners_name = []
+    participants = []
 
-match_detail = watcher.match.by_id(my_region, last_match['gameId'])
+    for row in match_detail['participantIdentities']:
+        names_row = {}
+        names_row['Name'] = row['player']['summonerName']
+        summoners_name.append(names_row)
+    name = [x['Name'] for x in summoners_name]
+    print(name)
+    for row in match_detail['participants']:
+        participants_row = {}
+        # participants_row['champion'] = row['championId']
+        # participants_row['spell1'] = row['spell1Id']
+        # participants_row['spell2'] = row['spell2Id']
+        # participants_row['win'] = row['stats']['win']
+        participants_row['kills'] = row['stats']['kills']
+        participants_row['deaths'] = row['stats']['deaths']
+        participants_row['assists'] = row['stats']['assists']
+        # participants_row['totalDamageDealt'] = row['stats']['totalDamageDealt']
+        # participants_row['goldEarned'] = row['stats']['goldEarned']
+        # participants_row['champLevel'] = row['stats']['champLevel']
+        # participants_row['totalMinionsKilled'] = row['stats']['totalMinionsKilled']
+        # participants_row['item0'] = row['stats']['item0']
+        # participants_row['item1'] = row['stats']['item1']
+        participants.append(participants_row)
 
-print(match_detail)
-participants = []
+    df = pd.DataFrame(participants, index=name)
+    print(df)
 
-for row in match_detail['participants']:
-    participants_row = {}
-    participants_row['champion'] = row['championId']
-    participants_row['spell1'] = row['spell1Id']
-    participants_row['spell2'] = row['spell2Id']
-    participants_row['win'] = row['stats']['win']
-    participants_row['kills'] = row['stats']['kills']
-    participants_row['deaths'] = row['stats']['deaths']
-    participants_row['assists'] = row['stats']['assists']
-    # participants_row['totalDamageDealt'] = row['stats']['totalDamageDealt']
-    participants_row['goldEarned'] = row['stats']['goldEarned']
-    participants_row['champLevel'] = row['stats']['champLevel']
-    participants_row['totalMinionsKilled'] = row['stats']['totalMinionsKilled']
-    participants_row['item0'] = row['stats']['item0']
-    participants_row['item1'] = row['stats']['item1']
-    participants.append(participants_row)
+    # latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
+    # static_champ_list = watcher.data_dragon.champions(latest, False, 'en_US')
 
-df = pd.DataFrame(participants)
-print(df)
+    # champ_dict = {}
 
-latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
-static_champ_list = watcher.data_dragon.champions(latest, False, 'en_US')
+    # for key in static_champ_list['data']:
+    #   row = static_champ_list['data'][key]
+    #   champ_dict[row['key']] = row['id']
 
-champ_dict = {}
+    # for row in participants:
+    #   print(str(row['champion']) + ' ' + champ_dict[str(row['champion'])])
+    #  row['championName'] = champ_dict[str(row['champion'])]
 
-for key in static_champ_list['data']:
-    row = static_champ_list['data'][key]
-    champ_dict[row['key']] = row['id']
+    ax = df
+    ax.plot(kind='bar')
+    plt.show()
+    '''
+    (MID_LANE, SOLO):MIDDLE
+    (TOP_LANE, SOLO):TOP
+    (JUNGLE, NONE):JUNGLE
+    (BOT_LANE, DUO_CARRY):BOTTOM
+    (BOT_LANE, DUO_SUPPORT):UTILITY
+    '''
 
-for row in participants:
-    print(str(row['champion']) + ' ' + champ_dict[str(row['champion'])])
-    row['championName'] = champ_dict[str(row['champion'])]
 
-df = pd.DataFrame(participants)
-print(df)
-ax = df
-ax.plot(kind='bar')
-plt.show()
-'''
-(MID_LANE, SOLO):MIDDLE
-(TOP_LANE, SOLO):TOP
-(JUNGLE, NONE):JUNGLE
-(BOT_LANE, DUO_CARRY):BOTTOM
-(BOT_LANE, DUO_SUPPORT):UTILITY
-'''
+if __name__ == "__main__":
+    main()
