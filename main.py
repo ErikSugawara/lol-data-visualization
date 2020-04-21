8d54e45dab7a774333fa4302c4ecacf190ce1adb
ErikSugawara <erik.sugawara@grad.ufsc.br>
2020-04-20 22:08:34 -0300

    Add wards score

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ from user import User
 class LolDataVisualization:
 
     def __init__(self):
-        api_key = 'RGAPI-e0b0f3de-3b0e-4fe5-9e1d-21e3c4f90715'
+        api_key = 'RGAPI-fe47e041-5624-4d72-a9b3-b3f02d17e6aa'
         self.watcher = LolWatcher(api_key)
 
     def plot_chart(self):
-        user = User('br1', 'Magafo', self.watcher)
+        user = User('br1', 'jean mage', self.watcher)
         kda_df = user.kda()
         kda_df.plot(kind='bar')
         dmg_df = user.damage_dealt_mitigated()
         dmg_df.plot(kind='bar')
+        ward_df = user.ward_score()
+        ward_df.plot(kind='bar')
         plt.show()
 
 
--- user.py
+++ user.py
@@ -13,56 +13,42 @@ class User:
         '''
         Returns Dataframe of KDA in participant matche
         '''
-        # List of participants name/info
-        summoners_name = []
-        participants = []
-
-        # Getting names of participants
-        for row in self.match_detail['participantIdentities']:
-            names_row = {}
-            names_row['Name'] = row['player']['summonerName']
-            summoners_name.append(names_row)
-        name = [x['Name'] for x in summoners_name]
-
         # Getting KDA
+        participants = []
         for row in self.match_detail['participants']:
             participants_row = {}
             participants_row['kills'] = row['stats']['kills']
             participants_row['deaths'] = row['stats']['deaths']
             participants_row['assists'] = row['stats']['assists']
             participants.append(participants_row)
-
-        df = pd.DataFrame(participants, index=name)
+        df = pd.DataFrame(participants, index=self.name)
         kda_values = (df['kills'] + df['assists'])/df['deaths']
         df['KDA'] = pd.DataFrame(kda_values, index=df.index)
-
         print(df)
-
         return df
 
     def damage_dealt_mitigated(self):
-        # List of participants name/info
-        summoners_name = []
-        participants = []
-
-        # Getting names of participants
-        for row in self.match_detail['participantIdentities']:
-            names_row = {}
-            names_row['Name'] = row['player']['summonerName']
-            summoners_name.append(names_row)
-        name = [x['Name'] for x in summoners_name]
-
         # Getting KDA
+        participants = []
         for row in self.match_detail['participants']:
             participants_row = {}
             participants_row['Total DMG Champions'] = row['stats']['totalDamageDealtToChampions']
             participants_row['Damage mitigated'] = row['stats']['damageSelfMitigated']
             participants.append(participants_row)
-
-        df = pd.DataFrame(participants, index=name)
-
+        df = pd.DataFrame(participants, index=self.name)
         print(df)
+        return df
 
+    def ward_score(self):
+        participants = []
+        for row in self.match_detail['participants']:
+            participants_row = {}
+            participants_row['Placed'] = row['stats']['wardsPlaced']
+            participants_row['Removed'] = row['stats']['wardsKilled']
+            participants_row['Vision Score'] = row['stats']['visionScore']
+            participants.append(participants_row)
+        df = pd.DataFrame(participants, index=self.name)
+        print(df)
         return df
 
     def match_information(self):
@@ -72,4 +58,14 @@ class User:
         last_match = self.matches['matches'][0]
         self.match_detail = self.watcher.match.by_id(self.region,
                                                      last_match['gameId'])
-        print(self.match_detail['participants'])
+        print(self.match_detail)
+        # List of participants name/info
+        summoners_name = []
+        # Getting names of participants
+        for row in self.match_detail['participantIdentities']:
+            names_row = {}
+            names_row['Name'] = row['player']['summonerName']
+            summoners_name.append(names_row)
+        self.name = [x['Name'] for x in summoners_name]
+
+
