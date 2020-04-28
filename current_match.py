4139fea39c4384b35fa7fe447b93a0a60cf49a1e
ErikSugawara <erik.sugawara@grad.ufsc.br>
2020-04-28 18:01:30 -0300

    Added class current match

---
+++ current_match.py
@@ -0,0 +1,42 @@
+from riotwatcher import ApiError
+class CurrentMatch:
+
+    def __init__(self, region, summonerId, watcher):
+        self.region = region
+        self.summonerId = summonerId
+        self.watcher = watcher
+        self.current_match_information()
+
+    def current_match_information(self):
+        try:
+            self.current_match = self.watcher.spectator.by_summoner(self.region,
+                                                                    self.summonerId)
+            self.participants_information()
+        except ApiError:
+            print("User is not current in a match")
+
+    def participants_information(self):
+        self.current_participants = []
+        self.current_participants_info = []
+        self.matches_participants = []
+        for row in self.current_match['participants']:
+            self.current_participants.append(row['summonerName'])
+
+        for participant_name in self.current_participants:
+            participant_info = self.watcher.summoner.by_name(self.region, participant_name)
+            self.current_participants_info.append(participant_info)
+
+        for participant_id in self.current_participants_info:
+            matches = self.watcher.match.matchlist_by_account(self.region, participant_id['accountId'])['matches']
+            self.matches_participants.append(matches)
+        print(self.current_participants_info)
+
+    def win_match_probability(self):
+        # Parameters: (KDA, Vision Score, Farm, Dmg Dealt/Mitigate)
+        # of 5 last game of each participant.
+        pass
+
+    # TODO: Get data from last five games, using current role and game mode classic.
+    # TODO: Function to get average of each paramenter in a match
+    def win_lane_probability(self):
+        pass
